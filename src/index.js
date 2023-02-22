73d6e130f676616fab5b24584560c073c23da9e2
Alisa <alisa.luoma@hotmail.fi>
2023-02-22 11:51:19 +0200

    Parentin components

--- src/index.js
+++ src/index.js
@@ -2,6 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function MainHeader(){
+  return <h1>REACT PRACTICE</h1>
+}
+
+function SubHeader(){
+  return <p>This will be an exciting!</p>
+}
+
+function Header(){
+  return(
+    <div>
+      <MainHeader></MainHeader>
+      <SubHeader></SubHeader>
+    </div>
+  )
+}
+
+
+function MainBody() {
+  return (
+    <div>
+      <p>We will learn React by buildin TaskOPedia!</p>
+      <ul>
+        <li>Dance</li>
+        <li>Eat dinner</li>
+      </ul>
+    </div>
+  )
+}
+
+function Footer() {
+  return <p>Happy Coding!</p>
+}
 root.render(
-<div>He</div>
+  <div>
+    <Header/>
+    <MainBody/>
+    <Footer/>
+  </div>
 );
