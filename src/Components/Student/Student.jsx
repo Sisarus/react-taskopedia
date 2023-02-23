464172b7c3e6c6e8b77994c3360db8d8bf7e7a58
Alisa <alisa.luoma@hotmail.fi>
2023-02-23 08:40:06 +0200

    little styling

--- src/Components/Student/Student.jsx
+++ src/Components/Student/Student.jsx
@@ -1,20 +1,24 @@
-const Student = (props) => {
-  return (
-    <div className='col-4 p-1'>
-      <div className='row border'>
-        <div className='col-2'>
-          <img src={props.headshot} className="w-100 py-2"></img>
-        </div> 
-        <div className='col-8'>
-          {props.name}<br/>
-          Programming Experience {props.experience} years
-        </div>
-        <div className="col-2">
-          {props.children}
+import React from "react";
+
+class Student extends React.Component{
+  render(){
+    return (
+      <div className='col-4 p-1'>
+        <div className='row border'>
+          <div className='col-2'>
+            <img src={this.props.headshot} className="w-100 py-2" alt="Person"></img>
+          </div> 
+          <div className='col-8'>
+            {this.props.name}<br/>
+            Programming Experience {this.props.experience} years
+          </div>
+          <div className="col-2">
+            {this.props.children}
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Student;
--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -3,7 +3,7 @@ import logo from "../images/react.png";
 function MainHeader(){
   return (
     <div className="pt-3 py-1 pl-2" style={{background: 'black'}}>
-      <img src={logo}  style={{height: "35px", verticalAlign: "top"}}></img>
+      <img src={logo}  style={{height: "35px", verticalAlign: "top"}} alt="React Logo"></img>
       <span className="h2 pt-4 text-white-50">REACT PRACTICE</span>
     </div>
   )
